b0a22fa74eaad4af3cd05e7a8c66cb55fbe54f5c
shota-hope <ddeeppaa.gogo@gmail.com>
2023-05-06 00:35:38 +0900

    Next.js と Rails の連携 #7

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -52,7 +52,9 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  config.hosts << 'localhost'
+  config.hosts << '127.0.0.1'
+  config.hosts << ENV['BACKEND_HOST']
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
